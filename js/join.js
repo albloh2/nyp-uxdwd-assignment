bd6ec870d5eaeee4739c2fae46b4ef2ee4fa9b0f
albloh2 <albloh2@gmail.com>
2025-08-10 17:43:10 +0800

    improve checking

--- js/join.js
+++ js/join.js
@@ -7,12 +7,19 @@ const STRINGS = {
     "phone": "Phone Number"
 };
 
+const STRINGS_ERROR = {
+    "fullname": "Full Name - Please enter your full name",
+    "admin_no": "NYP Admission Number - Please enter an valid NYP Admission Number",
+    "email": "Email Address - Please enter a valid email address",
+    "phone": "Phone Number - Please enter a valid Singapore phone number"
+};
+
 function handle_validation_single(event) {
     let element = event.srcElement;
     let type = element.id;
     let value = element.value;
     let error = false;
-    let labelElement = document.querySelector(`label[for="${inputId}"]`);
+    let labelElement = document.querySelector(`label[for="${type}"]`);
     if (type == "fullname") {
         let regex = /^[a-zA-Z\s-']*$/;
         if (value.length < 2 || value > 100) {
@@ -36,15 +43,22 @@ function handle_validation_single(event) {
         }
     } else if (type == "phone") {
         const regex = /^[689]\d{7}$/;
-        if (regex.test(value)) {
+        if (regex.test(value) == false) {
             error = true;
         }
     } else {
-        return
+        return True;
     }
     if (error) {
-        labelElement.innertext;
+        console.log(type)
+        labelElement.innerText = STRINGS_ERROR[type];
+    } else {
+        labelElement.innerText = STRINGS[type];
     }
 }
 
-document.getElementsByTagName("input")[0].onblur = handle_validation_single;
+
+for (let i = 0; i < input_boxes.length; i++) {
+    input_boxes[i].onblur = handle_validation_single;
+}
+
